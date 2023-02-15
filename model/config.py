77e465584a9d085e901ddc958619c2ed7cea9a83
Kevinsmm <mmsun0826@gmail.com>
2023-02-14 23:13:04 -0800

    add model config file

---
+++ model/config.py
@@ -0,0 +1,10 @@
+detection_model_path = "./model/saved_model/CNNAE_12layers_with_noise_valid.pt"
+classification_model_path = "./model/saved_model/CNNclf_4layers_valid.pt"
+detection_threshold = 40
+category2numerical = {
+    "hunger" : 0,
+    "pain" : 1,
+    "burping" : 2,
+    "discomfort" : 3,
+    "tired" : 4
+}
